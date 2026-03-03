4d45e90e47beee605176b58495d48b1c4cb342bb
developerplus2026 <anpham08112009@gmail.com>
2026-03-03 05:53:12 -0500

    Refactor Home component layout for improved alignment and readability

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,20 @@ import { Badge } from "@/components/ui/badge";
 import { ChevronRight } from "lucide-react";
 export default function Home() {
   return (
-    <div className="flex h-[calc(100vh-56px)] w-full items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex h-full  w-full flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black ">
-        <div className="flex flex-col items-center gap-8">
-          <div className="flex flex-col gap-8 items-center">
+    <div className="flex h-[calc(100vh-56px)] w-full items-center pl-24 justify-start bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex h-full  w-full flex-col items-start justify-start py-32 px-16 bg-white dark:bg-black ">
+        <div className="flex flex-col items-start gap-8">
+          <div className="flex flex-col gap-8 items-start">
             <div>
               <Badge variant={"outline"}>
                 {" "}
                 Introducing AgentMatic Version 2.0.3 <ChevronRight />
               </Badge>
             </div>
-            <h1 className=" text-center text-7xl w-240 text-balance">
+            <h1 className=" text-start text-7xl w-240 text-balance">
               The AI Agent for Modern Software Development
             </h1>
-            <p className="text-[#a1a1a1] text-center text-lg w-160 text-balance">
+            <p className="text-[#a1a1a1] text-start text-lg w-160 text-balance">
               A powerful AI-driven assistant designed specifically for
               developers. It helps generate clean code, explain complex logic,
               fix errors, optimize performance, and streamline daily development
