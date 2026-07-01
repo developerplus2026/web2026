fc22c6e1bce42023c34a64b93c23821fe14d43cb
developerplus2026 <anpham08112009@gmail.com>
2026-07-01 09:37:55 +0700

    fix: update padding in NavigationMenuBar for improved layout

--- components/navigation_menu_bar.tsx
+++ components/navigation_menu_bar.tsx
@@ -57,7 +57,7 @@ const components: { title: string; href: string; description: string }[] = [
 
 export function NavigationMenuBar() {
   return (
-    <div className="fixed border-b  border-input px-8 z-60 w-full  ">
+    <div className="fixed border-b  border-input px-19 z-60 w-full  ">
       <div className="  justify-between flex   items-center  h-14">
         <Link
           href="/"
