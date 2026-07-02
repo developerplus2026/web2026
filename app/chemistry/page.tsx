5d8784e3a22f77b1a2f44df587a162656d8f5de6
developerplus2026 <anpham08112009@gmail.com>
2026-07-02 10:56:42 +0700

    fix: update video source in PlayerUI component on HomePage

--- app/chemistry/page.tsx
+++ app/chemistry/page.tsx
@@ -2,9 +2,8 @@ import { PlayerUI } from "@/components/player";
 
 export default function HomePage() {
   return (
-    <div>
-      <h1>Welcome to My App</h1>
-      <PlayerUI src="https://player.mux.com/EUtAU6fZIg01mf4sU007fs0002W01HjE6xeDFHlO5VDNxfwA" />
+    <div className="mt-24 ">
+      <PlayerUI src="https://stream.mux.com/BV3YZtogl89mg9VcNBhhnHm02Y34zI1nlMuMQfAbl3dM/highest.mp4" />
     </div>
   );
 }
