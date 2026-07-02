eed89e89d22ed7f56cca774f84c62dc548fdfc5f
developerplus2026 <anpham08112009@gmail.com>
2026-07-02 10:43:25 +0700

    feat: add PlayerUI component for video playback functionality in HomePage

---
+++ app/chemistry/page.tsx
@@ -0,0 +1,10 @@
+import { PlayerUI } from "@/components/player";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      <PlayerUI src="https://player.mux.com/EUtAU6fZIg01mf4sU007fs0002W01HjE6xeDFHlO5VDNxfwA" />
+    </div>
+  );
+};
