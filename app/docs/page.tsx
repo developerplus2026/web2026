1960bba8ec7769c9f005454f6512458248a8392c
developerplus2026 <anpham08112009@gmail.com>
2026-07-02 17:55:23 +0700

    style: add height class to HomePage layout for consistent design

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,7 +2,7 @@ import { PageSlide } from "@/components/transition_ui";
 
 export default function HomePage() {
   return (
-    <div className="pt-38 w-full flex justify-center items-center  ">
+    <div className="pt-38 w-full h-100 flex justify-center items-center  ">
       <PageSlide />
     </div>
   );
