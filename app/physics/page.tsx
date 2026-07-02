0c7e81248bff915aa3a62dc4e876acaa63ebe8ed
developerplus2026 <anpham08112009@gmail.com>
2026-07-02 17:18:54 +0700

    style: add HomePage component with PlayerUI and video file

---
+++ app/physics/page.tsx
@@ -0,0 +1,9 @@
+import { PlayerUI } from "@/components/player";
+
+export default function HomePage() {
+  return (
+    <div className="pt-48 w-full flex justify-center items-center  ">
+      <PlayerUI src="/letters-comming-soon.mp4" />
+    </div>
+  );
+}
