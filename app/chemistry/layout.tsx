344553b448eaf3e45ed525fcfbf8a1a32b9649ab
developerplus2026 <anpham08112009@gmail.com>
2026-07-02 10:58:44 +0700

    refactor: remove unused ChemistryLayout component

--- app/chemistry/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from "react";
-
-interface ChemistryLayoutProps {
-  children: ReactNode;
-}
-
-export default function ChemistryLayout({ children }: ChemistryLayoutProps) {
-  return <main className="min-h-screen">{children}</main>;
-}
