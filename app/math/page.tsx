776ff276a4df0ed32247804b35b8ab902dd5d9e5
developerplus2026 <anpham08112009@gmail.com>
2026-08-11 21:05:48 +0700

    style: update video source in PlayerUI for HomePage

--- app/math/page.tsx
+++ app/math/page.tsx
@@ -3,7 +3,7 @@ import { PlayerUI } from "@/components/player";
 export default function HomePage() {
   return (
     <div className="pt-38 w-full flex justify-center items-center  ">
-      <PlayerUI src="/videos/letters-comming-soon.mp4" />
+      <PlayerUI src="https://vercel-player-six.vercel.app/play?v=019f453bd02d7ed8832017c94caa5a91&k=2026%2F03%2F11%2Fresources%2Fvideo%2Fph_n_1.1___l__thuy_t_v__d____t1_a1_1773199405700623643215.mp4" />
     </div>
   );
 }
